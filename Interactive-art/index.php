d027458f7e9860650942acbcad01c153a42c0a3b
@hellowritecode <82076385+tayyab7572@users.noreply.github.com>
2021-04-30 15:01:17 -0700

    Add files via upload

---
+++ Interactive-art/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Electronic Website</title>
+</head>
+<link rel="stylesheet" href="../css/style.css">
+<body>
+    <div id="container">
+    <div class="menu">
+       the menu
+    </div>
+    <div class="main-content">
+        main content
+    </div>
+    <div class="footer">
+        footer
+    </div>
+    </div>
+   
+</body>
+</html>
